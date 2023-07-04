1a46b5fd61be126fb6507356046ac7def5820b78
Abdullah Waseem <l201165@lhr.nu.edu.pk>
2023-07-04 23:25:08 +0500

    Updated : Added counter functionality using states using useReduce

--- src/screen/CounterScreen.js
+++ src/screen/CounterScreen.js
@@ -1,8 +1,20 @@
 import React from "react";
 import { Text,View, StyleSheet, Button } from "react-native";
-
+//using useReduce
+const reducer = (state,action) => {
+switch (action.type) {
+    case "increase":
+        return {counter : state.counter + 1}
+    case "decrease":
+        return {counter : state.counter - 1}
+    default:
+        break;
+}
+}
 const CounterScreen = ()=> {
-    const [counter, setCounter] = React.useState(0);
+    // const [counter, setCounter] = React.useState(0);
+    const [state, dispach] = React.useReducer(reducer , {counter : 0})
+    const {counter} = state;
     function counterPlus()
     {
         setCounter(counter + 1)
@@ -15,13 +27,13 @@ const CounterScreen = ()=> {
         <View>
             <Button 
             title="Counter ++"
-            onPress={counterPlus}
+            onPress={() => {dispach( {type:"increase"})}}
             />
             <Button 
             title="Counter --"
-            onPress={counterMin}
+            onPress={() => {dispach( {type:"decrease"})}}
             />
-            <Text>Counter {counter}</Text>
+            <Text>Counter {state.counter}</Text>
         </View>
     )
 }
